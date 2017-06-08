e5a21369db795054c6faa1ad9659abb9a002f059
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2017-06-08 15:15:47 -0700

    added views

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 
-var express = require('express'),
+const express = require('express'),
   config = require('./config/config');
 
-var app = express();
+const app = express();
+const handlebars = require('handlebars');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-module.exports = require('./config/express')(app, config);
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+app.get('/', (req, res) => {
+  res.render('admin')
+})
+
+app.get('/measurements', (req, res) => {
+  res.render('measurements')
+})
+
+app.get('/user_details', (req, res) => {
+  res.render('user_details')
+})
+
+app.get('/user_school_data', (req, res) => {
+  res.render('user_school_data')
+})
+
+
+
+// module.exports = require('./config/express')(app, config);
 
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
