96f8706e183f65edb0e3c26cb1914ecc48ab5f5b
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2017-06-09 15:00:24 -0700

    added view for user-measurements and got data to render from DB

--- app.js
+++ app.js
@@ -7,32 +7,22 @@ const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
+const Resoltzapi = require('./app/models/article').Resoltzapi;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, '/public')))
 
-
 app.get('/', (req, res) => {
   res.render('admin')
 })
 
 app.get('/measurements', (req, res) => {
-  res.render('measurements')
+  Resoltzapi.getAllUsersMeasurements().then( results => {
+    return res.render('measurements', {results})
+  })
 })
-
-app.get('/user_details', (req, res) => {
-  res.render('user_details')
-})
-
-app.get('/user_school_data', (req, res) => {
-  res.render('user_school_data')
-})
-
-
-
-// module.exports = require('./config/express')(app, config);
 
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
--- app/models/article.js
+++ app/models/article.js
@@ -2,40 +2,17 @@ const promise = require( 'bluebird' )
 const options = { promiseLib: promise }
 
 const pgp = require( 'pg-promise' )( options )
-const CONNECTION_STRING = `postgres://apigene@resoltz-api-pg:Resoltz21@resoltz-api-pg.postgres.database.azure.com:5432/resoltz-blog?ssl=true`
+const CONNECTION_STRING = `postgres://apigene@resoltz-api-pg:Resoltz21@resoltz-api-pg.postgres.database.azure.com:5432/resoltzapi?ssl=true`
 const db = pgp( CONNECTION_STRING )
 
-const getAllPosts = () =>
-  db.any( "SELECT * FROM blogPosts ORDER BY post_date ASC" )
+const Resoltzapi = {
+  getAllUsersMeasurements: () => {
+    let measurements = db.any(`SELECT * FROM measurements ORDER BY AgendaSlotId ASC`,[])
+    return measurements
 
-const getPost = ( id ) =>
-  db.one( `SELECT * FROM blogPosts where id=$1`, [ id ] )
-
-const createPost = ( { title, author, snippet, blog_entry, image_url, tags } ) =>
-  db.any(
-    `INSERT INTO blogPosts
-       ( title, author, snippet, blog_entry, image_url, tags )
-    VALUES
-      ( $1, $2, $3, $4, $5, $6 )`,
-    [ title, author, snippet, blog_entry, image_url, tags ] )
-
-const editPost = ( id, post ) => {
-  return db.oneOrNone( `UPDATE
-  blogPosts SET title=$2, author=$3, snippet=$4, blog_entry=$5, image_url=$6,
-  tags=$7  WHERE id=$1`,
-  [ id, post.title, post.author, post.snippet, post.blog_entry,
-  post.image_url, post.tags ] )
+  }
 }
-
-const deletePost = id =>
-  db.none( `DELETE FROM blogPosts WHERE id=$1`, [ id ] )
-
-
-module.exports = {
-  db,
-  getAllPosts,
-  getPost,
-  createPost,
-  editPost,
-  deletePost
-}
+Resoltzapi.getAllUsersMeasurements().then( results => {
+  console.log(results);
+})
+module.exports = {Resoltzapi}
